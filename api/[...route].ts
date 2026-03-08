b3e95366cb56eb31993b2cb3b926f7833f766a8b
PeterFujiyu <jiyu.fu@outlook.com>
2026-03-08 10:55:20 +0800

    feat: Migrate API to Hono on Vercel and include server files in the function build.

--- api/[...route].ts
+++ api/[...route].ts
@@ -1,1 +1,8 @@
-export { config, default } from './index.js'
+import { handle } from 'hono/vercel'
+import { createApp } from '../server/app.js'
+
+export const config = {
+  runtime: 'nodejs',
+}
+
+export default handle(createApp())
